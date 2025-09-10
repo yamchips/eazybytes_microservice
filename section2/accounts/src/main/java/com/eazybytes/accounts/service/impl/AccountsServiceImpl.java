4afb4b90838a9a9170322d42ebad048cf914e92f
yamchips <wflunch@gmail.com>
2025-09-10 21:00:51 +0100

    Update audit columns using Spring Data

--- section2/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -42,8 +42,6 @@ public class AccountsServiceImpl implements IAccountsService {
                     "Customer already registered with given " + "mobile number " +
                             customerDTO.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -127,8 +125,6 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
         return newAccount;
     }
 }
